bda1f9cc6d325859c9d3985b545579af63d2cfb2
baker <783009838@qq.com>
2017-11-17 17:30:54 +0800

    lesson3-操作-training-1

---
+++ homework/lesson3/lesson3.js
@@ -0,0 +1,52 @@
+console.log("第二章 操作")
+console.log("\n")
+
+console.log("## 1算数操作符")
+var total = (1 + 4) * 5;
+var temp_fahrenheit = 95;
+var temp_celsius = (temp_fahrenheit - 32) / 1.8
+var year = 0;
+year ++;
+// 拼接字符串
+var message = "im feeling" + "happy";
+
+console.log(total);
+console.log(temp_fahrenheit);
+console.log(temp_celsius);
+console.log(year);
+console.log(message);
+console.log("10"+20);
+console.log(10 + 20);
+
+console.log("\n")
+console.log("## 2 条件语句")
+var a = false;
+var b = "";
+// '==' 认为 ""和false相等
+if(a == b){ console.log("a equals b")};
+if(a === b) { console.log("a equals b")};
+
+console.log("\n")
+console.log("## 3 逻辑运算符")
+console.log("&&  ||")
+
+console.log("\n")
+console.log("## 4 循环语句")
+var count = 1;
+do
+{
+	console.log(count);
+	count ++;
+}
+while (count < 11);
+
+var count2 = 1;
+do
+{
+	console.log(count2);
+}
+while (count < 1);
+
+
+
+
